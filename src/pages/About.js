751a3479e194b68b5c97e09248463ee657851ec1
Owen Burnett <oburnett127@gmail.com>
2021-10-24 13:54:54 -0500

    Added About.js and src/images folder for the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,21 +1,58 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-function Splash() {
+function About() {
   return (
-    <div className="flex items-center justify-center min-h-screen space-x-4">
-      <motion.h1
-        initial={{ x: -100, opacity: 0 }}
-        animate={{ x: 0, opacity: [0, 50, 100] }}
-        className="text-4xl font-bold text-white"
-      >
-        About
-      </motion.h1>
+    <div className="items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">About</h1>
+      <br />
+      <br />
       <div>
-        
+        <h2 className="text-3xl font-bold text-white">Developers</h2>
+        <br />
+        <ul className="text-2xl text-white">
+          <li>Ali Alizadeh</li>
+          <li>Paul Pavlinskyi</li>
+          <li>Owen Burnett</li>
+        </ul>
+        <br />
+        <br />
+        <h2 className="text-3xl font-bold text-white">Dependencies Used</h2>
+        <br />
+        <div className="min-h-screen ">
+          <div className="justify-center text-2xl text-white">
+            <label for="framermotionimage">Framer Motion</label>
+            <img
+              id="framermotionimage"
+              src="src/framermotionimage.jpeg"
+              alt="Framer Motion Logo"
+            />
+            <br />
+            <label for="reactqueryimage">React Query</label>
+            <img
+              id="reactqueryimage"
+              src="src/reactqueryimage.png"
+              alt="React Query Logo"
+            />
+            <br />
+            <label for="reactrouterdomimage">React Router DOM</label>
+            <img
+              id="reactrouterdomimage"
+              src="src/reactrouterdomimage.png"
+              alt="React Router DOM Logo"
+            />
+            <br />
+            <label for="reduxlogoimage">Redux</label>
+            <img
+              id="reduxlogoimage"
+              src="src/reduxlogoimage.png"
+              alt="Redux Logo"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Splash;
+export default About;
